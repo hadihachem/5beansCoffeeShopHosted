ebb7724e2c64f5eecdde71500e1d3d4fe00dea3f
hadihachem <hadi.eliashachem@gmail.com>
2026-04-03 15:37:46 +0300

    fixing reload problem

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import  { useState } from "react"
+import { useState, useEffect } from "react"
 import Navbar from "./Components/Navbar/Navbar"
-import { Route, Routes } from "react-router-dom"
+import { Route, Routes, useNavigate } from "react-router-dom"
 import Home from "./pages/Home/Home"
 import Footer from "./Components/Footer/Footer"
 import './App.css'
@@ -8,26 +8,33 @@ import About from "./pages/About/About"
 import NotFound from "./Components/NotFound/NotFound"
 import ScrollToTop from "./Components/ScrollToTop/ScrollToTop"
 import Location from "./Components/Location/Location"
-const App = () => {
 
-  const [showLogin, setShowLogin]= useState(false);
+const App = () => {
+  const [showLogin, setShowLogin] = useState(false);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const redirectPath = sessionStorage.getItem('redirectPath');
+    if (redirectPath && redirectPath !== '/') {
+      sessionStorage.removeItem('redirectPath');
+      navigate(redirectPath);
+    }
+  }, [navigate]);
 
   return (
     <>
-    <div className="app">
-      <Navbar setShowLogin={setShowLogin}/>
-      <ScrollToTop />
-      <Routes>
-        <Route path="/" element={ <Home/>}/>
-        <Route path="/about" element={ <About />}/>
-        <Route path="*" element={<NotFound />} />
-        <Route path="/location" element={<Location />} />
-      </Routes>
-
-    </div>
-    <Footer />
+      <div className="app">
+        <Navbar setShowLogin={setShowLogin}/>
+        <ScrollToTop />
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/about" element={<About />}/>
+          <Route path="/location" element={<Location />}/>
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>
+      <Footer />
     </>
-    
   )
 }
 
