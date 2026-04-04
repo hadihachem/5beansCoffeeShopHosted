daa1c3a6d35f634de15ed9fde55f64da03c94dd4
hadihachem <hadi.eliashachem@gmail.com>
2026-04-04 10:15:39 +0300

    fixing map url

--- src/Components/Location/Location.jsx
+++ src/Components/Location/Location.jsx
@@ -7,13 +7,13 @@ const Location = () => {
       id: 1,
       name: "Adma w Dafnah",
       address: "Main Street, Adma w Dafnah",
-      embedUrl: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3698.7101120243615!2d35.644648411545035!3d34.0213076192508!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x151f41004822b1cf%3A0x931ea87b8ee98e9!2s5%20Beans%20Coffee%20Shop!5e1!3m2!1sen!2slb!4v1752230812863!5m2!1sen!2slb"
+      embedUrl: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3306.8795283331465!2d35.64464841129425!3d34.021303173056786!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x151f41004822b1cf%3A0x931ea87b8ee98e9!2s5%20Beans%20Coffee%20Shop!5e1!3m2!1sen!2slb!4v1775286501433!5m2!1sen!2slb&maptype=roadmap"
     },
     {
       id: 2,
       name: "Dekwaneh",
       address: "Tower 44 Ground Floor, Dekwaneh",
-      embedUrl: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3704.857655377382!2d35.53902391154023!3d33.87997812669461!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x151f17004ecfd541%3A0x335199c556745060!2s5%20Beans%20Coffee%20Shop!5e1!3m2!1sen!2slb!4v1752230574207!5m2!1sen!2slb"
+      embedUrl: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3704.857655377382!2d35.53902391154023!3d33.87997812669461!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x151f17004ecfd541%3A0x335199c556745060!2s5%20Beans%20Coffee%20Shop!5e1!3m2!1sen!2slb!4v1752230574207!5m2!1sen!2slb&maptype=roadmap"
     }
   ];
 
